21b300eeaa9ee540ff21972c6b360dc931b3af29
m3shark <sharkhockey66@yahoo.com>
2016-01-02 17:01:54 -0500

    Create invino-cellartracker.user.js
    Initial commit of invino-cellartracker userscript

---
+++ invino-cellartracker.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Invino - CellarTracker Integration
+// @namespace    http://www.invino.com/
+// @version      0.1.0
+// @description  Display CellarTracker search button on Invino product pages
+// @match        https://www.invino.com/
+// @match        https://www.invino.com/wines/*
+// @grant        none
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+var products = document.querySelectorAll (".products-name");
+
+for (var J = 0;  J < products.length;  J++) {
+    var product = products[J];
+    //console.log(product);
+    //Get Product Name
+    var name  = product.textContent.split("\n")[1].replace(/^\s+/, '').replace(/\s+$/, '');
+    name = name.split("'").join("%27");
+    name = name.split('"').join("%22");
+    name = name.split(" ").join("+");
+    var url = "https://www.cellartracker.com/list.asp?fInStock=0&Table=List&iUserOverride=0&szSearch="+name;
+    var zNode       = document.createElement ('div');
+    zNode.innerHTML = '<button id="myButton" type="button" onclick="window.open(\''+url+'\');">'
+        + '<img src="http://www.cellartracker.com/favicon2.ico"></button>'
+    ;
+    zNode.setAttribute ('id', 'myContainer');
+    product.appendChild(zNode);
+    console.log(name);
+};
