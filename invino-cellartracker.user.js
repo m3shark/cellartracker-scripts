2e536b965d237b34e566f7818d02b3b378b4d4b0
m3shark <sharkhockey66@yahoo.com>
2016-01-04 19:39:49 -0500

    Update invino-cellartracker.user.js
    added wine-searcher link as well to the page

--- invino-cellartracker.user.js
+++ invino-cellartracker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Invino - CellarTracker Integration
 // @namespace    http://www.invino.com/
-// @version      0.1.2
+// @version      0.2.0
 // @description  Display CellarTracker search button on Invino product pages
 // @match        https://www.invino.com/
 // @match        https://www.invino.com/wines/*
@@ -20,11 +20,17 @@ for (var J = 0;  J < products.length;  J++) {
     name = name.split("'").join("%27");
     name = name.split('"').join("%22");
     name = name.split(" ").join("+");
-    var url = "https://www.cellartracker.com/list.asp?fInStock=0&Table=List&iUserOverride=0&szSearch="+name;
-    var zNode       = document.createElement ('div');
-    zNode.innerHTML = '<button id="myButton" type="button" onclick="window.open(\''+url+'\');">'
+    
+    //Create CellarTracker link button
+    var cturl = "https://www.cellartracker.com/list.asp?fInStock=0&Table=List&iUserOverride=0&szSearch="+name;
+    var wsurl = "http://www.wine-searcher.com/find/"+name;
+    var ctNode       = document.createElement ('div');
+    ctNode.innerHTML = '<button id="myButton" type="button" onclick="window.open(\''+cturl+'\');">'
         + '<img src="http://www.cellartracker.com/favicon2.ico"></button>'
+        + '<button id="myButton" type="button" onclick="window.open(\''+wsurl+'\');">'
+        + '<img src="http://sr1.wine-searcher.net/images/favicon.1.png" style="height:16px;"></button>'
     ;
-    zNode.setAttribute ('id', 'myContainer');
-    product.appendChild(zNode);
+    ctNode.setAttribute ('id', 'CellarTrackerNode');
+    ctNode.setAttribute ('style', 'display:inline;');
+    product.appendChild(ctNode);
 };
